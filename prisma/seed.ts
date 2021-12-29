001ef4054b76f0143fcc0f2ad5ce6fd8fb7a50e2
yapwf <email2ywf@yahoo.com>
2021-12-29 08:44:55 +0800

    Add files via upload

---
+++ prisma/seed.ts
@@ -0,0 +1,69 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Join the Prisma Slack',
+          content: 'https://slack.prisma.io',
+          published: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Nilu',
+    email: 'nilu@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Follow Prisma on Twitter',
+          content: 'https://www.twitter.com/prisma',
+          published: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Yap',
+    email: 'woonfoo.yap@borderlessgeek.io',
+    posts: {
+      create: [
+        {
+          title: 'Post title 1',
+          content: 'Post title 1 content',
+          published: true,
+        },
+        {
+          title: 'Post title 2',
+          content: 'Post title 1 content',
+        },
+      ],
+    },
+  },
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
